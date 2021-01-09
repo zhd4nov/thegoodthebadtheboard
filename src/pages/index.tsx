f91da2b8e01587bd72b47c9b1cb5e10a0c48a93d
zhd4nov <zhdanov@element-soft.com>
2021-01-09 22:17:58 +0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main>
+        <h1 className="text-xl font-bold">
+          The Good, The Bad & The Board
+        </h1>
+        <div >
+        </div>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  );
+}
