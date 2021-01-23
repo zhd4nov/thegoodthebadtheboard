121168f07f46aab5b3020fe90b2491f24971755b
zhdnv <prctmlm@gmail.com>
2021-01-23 21:37:23 +0300

    set up storybook

---
+++ src/components/common/button/Button.stories.tsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Meta } from '@storybook/react';
+import Button from './Button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+} as Meta;
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  label: 'Push button',
+  bg: 'blue',
+};
